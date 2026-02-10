5131f926f800a6a41f560699b445981dc4d76d79
ColtonKawamura <48538658+ColtonKawamura@users.noreply.github.com>
2026-02-10 20:12:24 +0000

    Deploying to gh-pages from @ ColtonKawamura/coltonkawamura.github.io@3c4135ae96c799309223fb507c125ed4d5b8cf61 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ColtonKawamura/coltonkawamura",
+    "data-repo": "ColtonKawamura/coltonkawamura.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
