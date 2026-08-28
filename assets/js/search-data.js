24f061c5d60da8427b06eb040a67baf3aa68f2bc
ColtonKawamura <48538658+ColtonKawamura@users.noreply.github.com>
2026-08-28 10:33:51 +0000

    Deploying to gh-pages from @ ColtonKawamura/coltonkawamura.github.io@cd561f17c1bf5d9600a1f6cc2dc91ffedf069743 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-scaling-and-trade-offs-in-multi-agent-autonomous-systems",
+        
+          title: "Scaling and Trade-offs in Multi-agent Autonomous Systems",
+        
+        description: "A deep dive into designing autonomous drone swarms with dimensional analysis and scaling laws",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/scaling-multi-agent-systems/";
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
