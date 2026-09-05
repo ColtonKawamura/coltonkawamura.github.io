60fc83344efefd7998e4c543294e899d7b3beecb
ColtonKawamura <48538658+ColtonKawamura@users.noreply.github.com>
2026-09-05 23:25:58 +0000

    Deploying to gh-pages from @ ColtonKawamura/coltonkawamura.github.io@cfa387023a98044c7e7408090f061723b9ce8b27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -519,7 +519,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=MWmZoaAAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
